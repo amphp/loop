c7fbdfb7125e03b5653b13db1950fb45344e8e1b
Niklas Keller <me@kelunik.com>
2017-08-18 09:24:33 +0200

    Actively warn if this component is used
    Just in case somebody follows older issues or blog posts.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -4,4 +4,9 @@ namespace Amp\Loop;
 
 use AsyncInterop\Loop;
 
+\trigger_error(
+    "You're using amphp/loop, which is deprecated. Please use amphp/amp instead.",
+    E_USER_WARNING
+);
+
 Loop::setFactory(new LoopFactory);
